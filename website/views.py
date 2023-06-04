c0415b800fba36ff141b9a9c7c4c5fa4cdfdf560
Won <whdgurasd@naver.com>
2023-06-04 12:48:06 +0900

    delete

--- website/views.py
+++ website/views.py
@@ -9,18 +9,38 @@ views = Blueprint('views', __name__)
 def home():
     if request.method == 'POST':
         name = request.form.get('name')
-        option = request.form.get('list')
+        type = None
+        action = request.form.get('action')
+        if action == '등록':
+            option = request.form.get('list')
+            if option == 'Ko':
+                type = '한식'
+            elif option == 'Jp':
+                type = '일식'
+            elif option == 'Us':
+                type = '양식'
+            elif option == 'Cn':
+                type = '중식'
 
-        if option == 'Ko':
-            type = '한식'
-        elif option == 'Jp':
-            type = '일식'
-        elif option == 'Us':
-            type = '양식'
-        else:
-            type = '중식'
+            new_food = Dish(name=name, type=type)
+            db.session.add(new_food)
+            db.session.commit()
+        elif action == '삭제':
+            option = request.form.get('list')
+            if option == 'Ko':
+                type = '한식'
+            elif option == 'Jp':
+                type = '일식'
+            elif option == 'Us':
+                type = '양식'
+            elif option == 'Cn':
+                type = '중식'
 
-        new_food = Dish(name=name, type=type)
-        db.session.add(new_food)
-        db.session.commit()
+            dish_to_delete = Dish.query.filter_by(name=name, type=type).first() 
+
+            if dish_to_delete:
+                db.session.delete(dish_to_delete)
+                db.session.commit()
+
     return render_template('home.html')
+
